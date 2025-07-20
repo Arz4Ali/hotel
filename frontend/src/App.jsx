d6ba6616a91179ae24d12b08de89eedd3f20f984
Arz4Ali <arzmd86@gamil.com>
2025-07-20 11:56:41 +0545

    Frontend chnaged

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,8 @@ import AllRooms from './pages/AllRooms';
 import RoomDetails from './pages/RoomDetails';
 import MyBookings from './pages/MyBookings';
 import HotelReg from './components/HotelReg';
+import Layout from './pages/hotelOwner/Layout';
+import Dashboard from './pages/hotelOwner/Dashboard';
 
 
 const App = () => {
@@ -24,6 +26,11 @@ const App = () => {
           <Route path='/rooms' element={<AllRooms />} />
           <Route path='/rooms/:id' element={<RoomDetails />} />
           <Route path='/my-bookings' element={<MyBookings />} />
+          <Route path='/owner' element={<Layout />}>
+
+            <Route index element={<Dashboard />} />
+
+          </Route>
         </Routes>
       </div>
 
